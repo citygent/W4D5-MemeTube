bf6472f5da12c0d2c116766be1689b8526bf3c49
James White <cyberyt@gmail.com>
2015-09-05 19:16:26 +0100

    File structure, DB & Table created

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry'
+require 'pg'
+
+before do 
+  @db = PG.connect(dbname: 'memetube', host: 'localhost')
+end
+
+after do
+  @db.close
+end
+
+#Home/Lander
+get '/' do
+  redirect '/index'
+end
+
+#INDEX - get, list all videos PAGE
+#NEW - get, returns a form PAGE
+#CREATE - post, route to create vid on database, redirects to show
+#SHOW - get, a showcase video PAGE with edit link and delete functionality
+#EDIT - route within show page? KISS: get PAGE with route to UPDATE
+#UPDATE - post, route to change fields in db
+#DELETE - post, route thaat deletes rows in db
+
